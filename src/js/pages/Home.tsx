f28f3829ae9d128c3080e512026ad6bbcb535a26
Sam Nicholls <sam.nicholls@drpgroup.com>
2020-07-28 06:54:18 +0100

    Getting the api hooked up

--- src/js/pages/Home.tsx
+++ src/js/pages/Home.tsx
@@ -1,23 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Home = (): JSX.Element => {
+  const [posts, setPosts] = useState<any[]>([])
+
+  useEffect(() => {
+    // Delay the useEffect by 1000ms
+    const timeout = setTimeout(() => {
+      // Fetch the posts data from the API
+      fetch('https://jsonplaceholder.typicode.com/posts/')
+        // Store the response
+        .then((response) => response.json())
+        // Only return the first 10 results
+        .then((data) => setPosts(data.slice(0, 10)))
+    }, 1000)
+    // Clear the timer when it's done
+    return () => clearTimeout(timeout)
+  }, [])
+
   return (
     <div className='l-page'>
       <h1 className='l-page__title'>Blog</h1>
-      <p className='l-page__description'>
-        Tellus adipiscing sequi dignissim odio impedit, distinctio, repudiandae
-        felis iure molestiae quod. Torquent ultricies, proident, duis blanditiis
-        facere, beatae, repudiandae pharetra itaque ultricies morbi laudantium
-        varius in saepe fames vel adipisicing, arcu, potenti. Suspendisse. Irure
-        aenean, dolore eius potenti officiis aspernatur porttitor curae
-        praesentium? Aliqua, nunc incidunt ut, qui augue. Suspendisse impedit?
-        Sapien doloribus tenetur, pede iaculis egestas? Hic asperiores dicta
-        quibusdam velit, cillum nisi unde, mauris massa ac, tristique dicta
-        modi! Blandit temporibus condimentum, placerat, eveniet temporibus nunc,
-        eiusmod! Accusantium unde consequat aliquet! Pretium quam pulvinar
-        ullamco sit ultrices id iaculis amet dolores officiis atque laborum
-        metus atque, nihil.
-      </p>
+      {posts.length > 0 ? (
+        <div className='l-grid'>
+          {posts.map((post) => (
+            <div className='c-card' key={post.id}>
+              <p className='c-card__title'>{post.title}</p>
+              <p className='c-card__description'>{post.body}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        // TODO: Add in Loading Spinner
+        <h1>Loading</h1>
+      )}
     </div>
   )
 }
